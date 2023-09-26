dbda7a4e875aea4268ee3dc0e792beadc0e3667e
VonC <vonc@laposte.net>
2023-09-27 01:05:21 +0200

    unroll array, apply the conditions, group it back
    MongoDB's aggregation pipeline works on a per-document basis.
    Since we used the $filter array operation, it keeps the whole document
    as-is when it matches any of the conditions.
    This means that it does not distinguish between multiple conditions
    that apply to different documents in the array.
    
    To handle this more complex filtering logic, we might need to unwind
    the array, apply the multiple conditions, and then
    re-group the documents.
    However, this would mean a completely different approach to your
    pipeline, which could potentially be less efficient for large arrays.

--- src/main/java/com/example/countrysearch/service/CountryService.java
+++ src/main/java/com/example/countrysearch/service/CountryService.java
@@ -1,12 +1,12 @@
 package com.example.countrysearch.service;
 
+import com.example.countrysearch.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.stereotype.Service;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+import org.springframework.data.mongodb.core.query.Criteria;
 
-import com.example.countrysearch.model.Country;
 import java.util.List;
 
 @Service
@@ -16,53 +16,52 @@ public class CountryService {
     private MongoTemplate mongoTemplate;
 
     public List<Country> findCountriesByCity(String searchTerm) {
-        AggregationExpression conditionAExpr = ConditionalOperators.when(
-                BooleanOperators.And.and(
-                        ComparisonOperators.Eq.valueOf("city.from").equalToValue(searchTerm),
-                        ComparisonOperators.Gt.valueOf("city.population").greaterThanValue(1000000)
-                )
-        ).thenValueOf("$$KEEP").otherwise(null);
 
-        AggregationExpression conditionBExpr = ConditionalOperators.when(
-                ComparisonOperators.Eq.valueOf("city.from").equalToValue(searchTerm)
-        ).thenValueOf("$$KEEP").otherwise(null);
+        // Define the conditional expressions for each case
+        ConditionalOperators.Cond aCondition = ConditionalOperators.when(
+                Criteria.where("cities.from").is(searchTerm).and("cities.population").gt(1000000))
+                .then("$$ROOT.cities")
+                .otherwise(null);
 
-        AggregationExpression conditionCExpr = ConditionalOperators.when(
-                ComparisonOperators.Eq.valueOf("city.detail").equalToValue("new_city")
-        ).thenValueOf("$$KEEP").otherwise(null);
+        ConditionalOperators.Cond bCondition = ConditionalOperators.when(
+                Criteria.where("cities.from").is(searchTerm))
+                .then("$$ROOT.cities")
+                .otherwise(null);
 
-        Aggregation aggregation = newAggregation(
-                project("id", "name", "cities")
-                        .and(ArrayOperators.Filter.filter("cities")
-                                .as("city")
-                                .by(conditionAExpr))
-                        .as("conditionACities")
-                        .and(ArrayOperators.Filter.filter("cities")
-                                .as("city")
-                                .by(conditionBExpr))
-                        .as("conditionBCities")
-                        .and(ArrayOperators.Filter.filter("cities")
-                                .as("city")
-                                .by(conditionCExpr))
-                        .as("conditionCCities"),
-                project("id", "name")
-                        .and(
-                                ConditionalOperators.when(
-                                        ComparisonOperators.Eq.valueOf(
-                                                ArrayOperators.Size.lengthOfArray("conditionACities")
-                                        ).equalToValue(0)
-                                ).thenValueOf(
-                                        ConditionalOperators.when(
-                                                ComparisonOperators.Eq.valueOf(
-                                                        ArrayOperators.Size.lengthOfArray("conditionBCities")
-                                                ).equalToValue(0)
-                                        ).then("$conditionCCities").otherwise("$conditionBCities")
-                                ).otherwise("$conditionACities")
-                        ).as("finalCities")
+        ConditionalOperators.Cond cCondition = ConditionalOperators.when(
+                Criteria.where("cities.detail").is("new_city"))
+                .then("$$ROOT.cities")
+                .otherwise(null);
+
+        // Unwind the cities
+        UnwindOperation unwind = Aggregation.unwind("cities");
+
+        // Apply the conditional logic
+        ProjectionOperation project = Aggregation.project()
+                .and(aCondition).as("caseA")
+                .and(bCondition).as("caseB")
+                .and(cCondition).as("caseC")
+                .and("name").as("name")
+                .and("_id").as("_id");
+
+        // Re-group by ID and name, prioritizing A over B over C
+        GroupOperation group = Aggregation.group("_id")
+                .first("name").as("name")
+                .push(ConditionalOperators.when(Criteria.where("caseA").ne(null)).then("caseA").otherwise(
+                        ConditionalOperators.when(Criteria.where("caseB").ne(null)).then("caseB").otherwise("caseC")
+                )).as("finalCities");
+
+        // Build the aggregation pipeline
+        Aggregation aggregation = Aggregation.newAggregation(
+                unwind,
+                project,
+                group,
+                Aggregation.project("name", "finalCities")
         );
 
-        AggregationResults<Country> result = mongoTemplate.aggregate(aggregation, "country", Country.class);
+        // Execute the aggregation
+        AggregationResults<Country> results = mongoTemplate.aggregate(aggregation, Country.class, Country.class);
 
-        return result.getMappedResults();
+        return results.getMappedResults();
     }
 }
