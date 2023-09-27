0bfe2816c81f853b71f72e6a5cf71d45072348b8
VonC <vonc@laposte.net>
2023-09-27 20:08:29 +0200

    AggregationExpressions for each condition a, b, c
    Key modifications:
    
    1. Added an initialProjection to project filtered arrays into
    temporary fields: tempCitiesA and tempCitiesB.
    2. Then, we compute the size of these filtered arrays
    using ArrayOperators.Size.lengthOfArray.
    3. Finally, used these sizes in conditional statements
    to determine which array to include in the output.

--- src/main/java/com/example/countrysearch/service/CountryService.java
+++ src/main/java/com/example/countrysearch/service/CountryService.java
@@ -4,7 +4,6 @@ import com.example.countrysearch.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.*;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,36 +16,42 @@ public class CountryService {
 
     public List<Country> findCountriesByCity(String cityName) {
 
-        // Unwind cities array
-        UnwindOperation unwind = Aggregation.unwind("cities");
-
-        // Perform conditional projection to determine cityPriority
-        ProjectionOperation projection = Aggregation.project("name", "cities")
-                .and(ConditionalOperators
-                        .when(Criteria.where("cities.from").is(cityName)
-                                .and("cities.population").gte(1000000))
-                        .then(true)
-                        .otherwise(ConditionalOperators
-                                .when(Criteria.where("cities.from").is(cityName))
-                                .then(true)
-                                .otherwise(false)
+        ProjectionOperation initialProjection = Aggregation.project("name", "cities")
+                .and(ArrayOperators.Filter.filter("cities").as("city").by(
+                        BooleanOperators.And.and(
+                                ComparisonOperators.Eq.valueOf("city.from").equalToValue(cityName),
+                                ComparisonOperators.Gt.valueOf("city.population").greaterThanValue(10)
                         )
-                ).as("cityPriority");
+                )).as("tempCitiesA")
+                .and(ArrayOperators.Filter.filter("cities").as("city").by(
+                        BooleanOperators.And.and(
+                                ComparisonOperators.Eq.valueOf("city.from").equalToValue(cityName),
+                                ComparisonOperators.Eq.valueOf("city.population").equalToValue(null)
+                        )
+                )).as("tempCitiesB");
 
-        // Group by _id and prepare the final list of cities
-        GroupOperation group = Aggregation.group("_id")
-                .first("name").as("name")
-                .push(Aggregation.ROOT).as("finalCities");
+        AggregationExpression sizeOfA = ArrayOperators.Size.lengthOfArray("tempCitiesA");
+        AggregationExpression sizeOfB = ArrayOperators.Size.lengthOfArray("tempCitiesB");
 
-        // Create aggregation pipeline
-        Aggregation aggregation = Aggregation.newAggregation(
-                unwind,
-                projection,
-                group
-        );
+        ConditionalOperators.Cond finalCondition = ConditionalOperators
+                .when(ComparisonOperators.Eq.valueOf(sizeOfA).equalToValue(1))
+                .then("$tempCitiesA")
+                .otherwise(
+                        ConditionalOperators
+                        .when(ComparisonOperators.Eq.valueOf(sizeOfB).equalToValue(1))
+                        .then("$tempCitiesB")
+                        .otherwise(ArrayOperators.Filter.filter("cities")
+                                .as("city")
+                                .by(ComparisonOperators.Eq.valueOf("city.detail").equalToValue("new_city")))
+                );
 
-        // Execute the aggregation
+        ProjectionOperation finalProjection = Aggregation.project("name")
+                .and(finalCondition).as("cities");
+
+        Aggregation aggregation = Aggregation.newAggregation(initialProjection, finalProjection);
+
         AggregationResults<Country> results = mongoTemplate.aggregate(aggregation, "country", Country.class);
+
         return results.getMappedResults();
     }
 }
